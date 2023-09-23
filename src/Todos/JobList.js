fb67ddd3de7802825b0309b3cf0bc35d693bd98f
philconal2411@gmail.com <philconal2411@gmail.com>
2023-09-23 22:37:54 +0700

    add

--- src/Todos/JobList.js
+++ src/Todos/JobList.js
@@ -4,7 +4,6 @@ function JobList() {
   const jobs = [1, 2, 2, 3];
   return (
     <>
-      <h1>Hello Conal</h1>
       {jobs.map((item, index) => {
         return <JobItem key={index}></JobItem>;
       })}
--- src/Todos/MyToDo.js
+++ src/Todos/MyToDo.js
@@ -1,29 +1,34 @@
-
-import React, {useReducer, useRef} from "react";
-import {initialize, reducer} from "./store/reducer";
-import {addJob, setJob} from "./store/actions";
+import React, { useReducer, useRef } from "react";
+import { initialize, reducer } from "./store/reducer";
+import { addJob, setJob } from "./store/actions";
 import Input from "./components/Input";
 import Button from "./components/Button";
 import JobList from "./components/JobList";
 
 function MyToDo() {
-    const inputRef = useRef(null);
-    const [state, dispatch] = useReducer(reducer, initialize)
-    const handleInputChange = (e) => {
-        dispatch(setJob(e.target.value));
-    };
-    const handleAddJob = () => {
-        dispatch(addJob(state.job));
-        dispatch(setJob(""));
-        inputRef.current?.focus();
-    };
-    return (
-        <>
-            <Input inputRef ={inputRef} state={state} inputChange={handleInputChange}/>
-            <Button onAddJob={handleAddJob}> </Button>
-            <JobList jobs={state.jobs}> </JobList>
-        </>
-    )
+  const inputRef = useRef(null);
+  const [state, dispatch] = useReducer(reducer, initialize);
+  const handleInputChange = (e) => {
+    dispatch(setJob(e.target.value));
+  };
+  const handleAddJob = () => {
+    dispatch(addJob(state.job));
+    dispatch(setJob(""));
+    inputRef.current?.focus();
+  };
+  return (
+    <>
+      <h1>Hello Conal</h1>
+
+      <Input
+        inputRef={inputRef}
+        state={state}
+        inputChange={handleInputChange}
+      />
+      <Button onAddJob={handleAddJob}> </Button>
+      <JobList jobs={state.jobs}> </JobList>
+    </>
+  );
 }
 
-export default MyToDo
+export default MyToDo;
